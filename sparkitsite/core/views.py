fffd0280ef5e09993ee06dd54155d69bd1ed1ebc
Victor Fontes <contato@victorfontes.com>
2012-01-31 18:27:25 -0300

    envio de emails

--- sparkitsite/core/views.py
+++ sparkitsite/core/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from annoying.decorators import render_to
+from templated_emails.utils import send_templated_email
 from forms import SparkCampForm
 
 @render_to('core/index.html')
@@ -10,6 +11,15 @@ def index(request):
 def spark_camp(request):
     form = SparkCampForm(request.POST or None)
     if form.is_valid():
-    	pass
-    
-    return locals()
+    	TO_EMAIL = ['victor@sparkit.com.br']
+		
+		nome_projeto 	= form.cleaned_data['nome_projeto']
+		url_projeto 	= form.cleaned_data['url_projeto']
+		desc_projeto 	= form.cleaned_data['desc_projeto']
+		time 			= form.cleaned_data['time']
+		email 			= form.cleaned_data['email']
+		celular 		= form.cleaned_data['celular']
+
+		send_templated_email(TO_EMAIL, 'emails/spark-campus', locals())
+
+	return locals()
--- sparkitsite/settings.py
+++ sparkitsite/settings.py
@@ -36,10 +36,18 @@ SITE_ID = 1
 
 ON_HEROKU = os.environ.has_key('DATABASE_URL')
 
-#if ON_HEROKU:
-#    from libs.configs.env_heroku import * 
+if ON_HEROKU:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' #EMAIL_BACKEND = 'django_mailer.smtp_queue.EmailBackend'
+    EMAIL_HOST = 'smtp.sendgrid.net'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+    EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+
 #else:
 #    from libs.configs.env_dev import * 
+
+
 
 
 
