9b75ca2664b97440053614e3ffc9acabba225fb5
Victor Fontes <contato@victorfontes.com>
2012-02-02 11:42:08 -0300

    email doidão

--- sparkitsite/core/views.py
+++ sparkitsite/core/views.py
@@ -22,13 +22,12 @@ def spark_camp(request):
 	ENVIADO = False
 	if form.is_valid():
 		TO_EMAIL = ['contato@sparkit.com.br']
-
 		nome 			= form.cleaned_data['nome_projeto']
 		nome_projeto 	= form.cleaned_data['nome_projeto']
 		url_projeto 	= form.cleaned_data['url_projeto']
 		desc_projeto 	= form.cleaned_data['desc_projeto']
 		time 			= form.cleaned_data['time']
-		email 			= form.cleaned_data['email']
+		email_2 			= form.cleaned_data['email']
 		celular 		= form.cleaned_data['celular']
 
 		send_templated_email(TO_EMAIL, 'emails/spark-campus', locals())
